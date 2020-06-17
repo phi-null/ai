1cf2a4aaffcfd30d7f7e57dabec56650706e800b
φ <l_nothing@icloud.com>
2020-06-18 03:47:27 +0900

    Update index.ts

--- src/modules/update/index.ts
+++ src/modules/update/index.ts
@@ -2,6 +2,8 @@ import autobind from 'autobind-decorator';
 import Module from '../../module';
 import Message from '../../message';
 
+const exec = require('child_process').exec;
+
 export default class extends Module {
 	public readonly name = 'update';
 
@@ -16,6 +18,10 @@ export default class extends Module {
 	private async mentionHook(msg: Message) {
 		if (msg.text && msg.includes(['アプデ', 'アップデート', 'update']) && msg.includes(['みすきー', 'ミスキー', 'misskey'])) {
 			if (msg.user.host == null && msg.user.isAdmin) {
+        exec('sh /home/misskey/update.sh' , (err , stdout, stderr) =>{
+          if(err){console.log(err);}
+          console.log(stdout);
+        })
 				return {
 					reaction: msg.friend.love >= 0 ? 'like' : null
 				};

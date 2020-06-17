9a84e30c5877dca027877959c93005124a6be10f
φ <l_nothing@icloud.com>
2020-06-18 01:39:57 +0900

    Add update function

---
+++ src/modules/update/index.ts
@@ -0,0 +1,33 @@
+import autobind from 'autobind-decorator';
+import Module from '../../module';
+import Message from '../../message';
+
+export default class extends Module {
+	public readonly name = 'update';
+
+	@autobind
+	public install() {
+		return {
+			mentionHook: this.mentionHook
+		};
+	}
+
+	@autobind
+	private async mentionHook(msg: Message) {
+		if (msg.text && msg.includes(['アプデ', 'アップデート', 'update']) && msg.includes(['みすきー', 'ミスキー', 'misskey'])) {
+			if (msg.user.host == 'misskey.void.gift' && msg.user.isAdmin) {
+				return {
+					reaction: msg.friend.love >= 0 ? 'like' : null ,
+          reply('アップデート実行します！');
+				};
+			} else {
+				return {
+					reaction: msg.friend.love >= 0 ? 'hmm' : null
+          reply('あなたにはこのコマンドの発行権限がありません！');
+				};
+			}
+		} else {
+			return false;
+		}
+	}
+}
